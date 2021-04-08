11d6e3e37a06ed3f5dc90c076d35d48969d7fbcd
Albert I <kras@raphielgang.org>
2021-04-09 06:18:44 +0800

    extra: Move dotOS OTA URL override from vendor/kud
      commit c1378fc52b9421e34d18973e2f4bfa4a022e2c06
      Author: Albert I <kras@raphielgang.org>
      Date:   Tue Mar 30 17:42:18 2021 +0800
    
          kud: Override dotOS OTA URL via overlay
    
          Signed-off-by: Albert I <kras@raphielgang.org>
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- product.mk
+++ product.mk
@@ -1,2 +1,7 @@
 # Copyright (C) 2021 KudProject Development
 # SPDX-License-Identifier: Apache-2.0
+
+VENDOR_OS := $(shell bash -c 'echo $(TARGET_PRODUCT) | cut -d "_" -f1')
+
+# Include targeted ROM overlays
+DEVICE_PACKAGE_OVERLAYS += vendor/extra/overlay-$(VENDOR_OS)
