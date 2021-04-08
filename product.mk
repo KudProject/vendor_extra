aa4c42bea60ab8125106bb39ef9f916ff7972c54
Albert I <kras@raphielgang.org>
2021-04-09 06:26:22 +0800

    extra: Move disable_blurs prop from vendor/kud
      commit 039f952ed0161ad641e503d42c55ccfafdc50052
      Author: Albert I <kras@raphielgang.org>
      Date:   Sun Mar 28 01:37:37 2021 +0800
    
          kud: Disable blur by default if TARGET_SUPPORTS_BLUR
    
          Signed-off-by: Albert I <kras@raphielgang.org>
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- product.mk
+++ product.mk
@@ -5,3 +5,9 @@ VENDOR_OS := $(shell bash -c 'echo $(TARGET_PRODUCT) | cut -d "_" -f1')
 
 # Include targeted ROM overlays
 DEVICE_PACKAGE_OVERLAYS += vendor/extra/overlay-$(VENDOR_OS)
+
+# Disable blur by default
+ifeq ($(TARGET_SUPPORTS_BLUR),true)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.sys.sf.disable_blurs=1
+endif
