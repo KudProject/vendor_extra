7fab26580587f1d5e75bc8444812d9be46932dbf
Albert I <kras@raphielgang.org>
2021-05-10 21:47:10 +0800

    extra: Inherit Google APEX configuration from vendor/prebuilts
    * Only if device ships with updatable APEX
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- product.mk
+++ product.mk
@@ -3,6 +3,16 @@
 
 VENDOR_OS := $(shell bash -c 'echo $(TARGET_PRODUCT) | cut -d "_" -f1')
 
+# Inherit Google APEX configuration
+ifeq ($(WITH_GAPPS),true)
+ifeq ($(filter true,$(OVERRIDE_TARGET_FLATTEN_APEX) $(TARGET_FLATTEN_APEX)),)
+ifeq ($(TARGET_FLATTEN_APEX),false)
+$(warning Building with Google APEX modules)
+$(call inherit-product-if-exists, vendor/prebuilts/config/apex.mk)
+endif
+endif
+endif
+
 # Include targeted ROM overlays
 DEVICE_PACKAGE_OVERLAYS += vendor/extra/overlay-$(VENDOR_OS)
 
