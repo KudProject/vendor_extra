876e83f0c6d31fe1c98ab4092f8af952b0a8c3c7
Albert I <kras@raphielgang.org>
2022-01-30 19:29:10 +0800

    extra: Inherit GMS Go configuration if needed

--- product.mk
+++ product.mk
@@ -9,3 +9,9 @@ DERP_BUILDTYPE := CI
 
 # Include KudProject signing keys configuration
 include vendor/kud/config/certs.mk
+
+# Inherit GMS Go configuration for devices with <= 2 GB RAM variant
+ifneq ($(filter %ulysse,$(TARGET_PRODUCT)),)
+WITH_GMS_GO := true
+$(call inherit-product, vendor/gms_go/products/gms_go.mk)
+endif
